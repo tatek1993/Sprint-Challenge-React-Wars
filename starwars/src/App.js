47ddb8456ce5325b9442825d18e73675a6d10ba5
Tate Keller <tatek1993@gmail.com>
2020-01-24 10:59:10 -0600

    setting up divs and api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import CharacterPage from "./components/CharacterPage";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
---
+++ starwars/src/components/CharacterPage.js
@@ -0,0 +1,10 @@
+import React, {
+    useState,
+    useEffect
+} from 'react';
+import axios from "axios";
+import CharacterTile from "./components/CharacterTile";
+
+function CharacterList() {
+    const [character, setCharacter] = useState([]);
+}
